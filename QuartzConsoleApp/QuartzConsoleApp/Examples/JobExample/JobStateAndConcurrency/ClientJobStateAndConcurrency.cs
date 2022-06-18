b71cada1227171d0bd9fb67912bfe815d0ca3510
Matheus Patrick <mpatrickaires@gmail.com>
2022-06-18 10:30:17 -0300

    Created Job state and concurrency example

---
+++ QuartzConsoleApp/QuartzConsoleApp/Examples/JobExample/JobStateAndConcurrency/ClientJobStateAndConcurrency.cs
@@ -0,0 +1,38 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzConsoleApp.Examples.JobExample.JobStateAndConcurrency
+{
+    public static class ClientJobStateAndConcurrency
+    {
+        public async static Task Run()
+        {
+            IScheduler scheduler = await new StdSchedulerFactory().GetScheduler();
+
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<ExampleJob>()
+                .WithIdentity("myJob", "myGroup")
+                .UsingJobData("name", "Andrew")
+                .UsingJobData("age", 26)
+                .UsingJobData("changedData", Guid.NewGuid())
+                .UsingJobData("injectedData", Guid.NewGuid())
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "myGroup")
+                .UsingJobData("triggerData", Guid.NewGuid())
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(5)
+                    .RepeatForever())
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            await Task.Delay(TimeSpan.FromSeconds(30));
+        }
+    }
+}
---
+++ QuartzConsoleApp/QuartzConsoleApp/Examples/JobExample/JobStateAndConcurrency/ExampleJob.cs
@@ -0,0 +1,69 @@
+﻿using Quartz;
+using QuartzConsoleApp.Examples.JobDataMapExample;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzConsoleApp.Examples.JobExample.JobStateAndConcurrency
+{
+    // Since the class now have the PersistJobDataAfterExecution attribute, data modified or added
+    // by the job instance will be kept in the JobDataMap, which means that it will be persisted
+    // between executions.
+    [PersistJobDataAfterExecution]
+    public class ExampleJob : IJob
+    {
+        public Guid InjectedData { get; set; }
+        public Task Execute(IJobExecutionContext context)
+        {
+            var logger = new JobLogger("JobExample.JobStateAndConcurrency");
+            logger.Log();
+            try
+            {
+                // If the changes are made at the JobDataMap obtained from the MergedJobDataMap,
+                // the changes will not be persisted.
+                //JobDataMap dataMap = context.MergedJobDataMap;
+                JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+                JobDataMap dataMapTrigger = context.Trigger.JobDataMap;
+
+                var name = dataMap.GetString("name");
+                var age = dataMap.GetInt("age");
+                var changedData = dataMap.GetGuid("changedData");
+                var triggerData = dataMapTrigger.GetGuid("triggerData");
+
+                Console.WriteLine($"Name: {name} - Age: {age}");
+                Console.WriteLine($"Changed Data: {changedData}");
+                Console.WriteLine($"Injected Data: {InjectedData}");
+                Console.WriteLine($"Trigger Data: {triggerData}");
+
+                dataMap.Put("changedData", Guid.NewGuid());
+
+                var newData = (string)dataMap.Get("newData") ?? "There is no new data :(";
+                Console.WriteLine($"New Data: {newData}");
+
+                dataMap.Put("newData", "It's me, the new data :)");
+
+                // Doing changes directly to the property that was injected with the data will not
+                // have affect at the persistance; it is necessary to do the changes directly at the
+                // JobDataMap.
+                InjectedData = Guid.NewGuid();
+
+                // Changes directly at the JobDataMap obtained from the ITrigger will also not be
+                // persisted.
+                // In conclusion, to save the additions and modifications, the job instance can only
+                // make changes to the JobDataMap obtained from the IJobDetail
+                dataMapTrigger.Put("triggerData", Guid.NewGuid());
+
+                Console.WriteLine();
+
+                return Task.CompletedTask;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Oops! An error ocurred during the execution of the job - " +
+                    $"{e.GetType()}: {e.Message}");
+                Console.WriteLine();
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
