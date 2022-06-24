093a4fdf2661f68fba492fc491cc5add6647485e
Matheus Patrick <mpatrickaires@gmail.com>
2022-06-23 22:53:51 -0300

    Created new project: QuartzConsoleAdoJobStore # For the understanding and practice of the AdoJobStore

---
+++ QuartzConsoleAdoJobStore/QuartzConsoleAdoJobStore/ExampleJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QuartzConsoleAdoJobStore
+{
+    public class ExampleJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"[{DateTime.Now}]  Job executed!");
+
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+
+            Console.WriteLine($"[{DateTime.Now}]  Job finished!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ QuartzConsoleAdoJobStore/QuartzConsoleAdoJobStore/Program.cs
@@ -0,0 +1,51 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System.Threading.Tasks;
+
+namespace QuartzConsoleAdoJobStore
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // IMPORTANT:
+            // Access the following page to see all the Quartz tables creation scripts respective
+            // for each database:
+            // https://github.com/quartznet/quartznet/tree/main/database/tables
+            //
+            // To Quartz access the database, you must install it respective drive through the Nuget.
+            // In this example, the Npgsql package was intalled.
+
+            var scheduler = await new StdSchedulerFactory().GetScheduler();
+
+            System.Console.WriteLine($"Scheduler Name: {scheduler.SchedulerName}");
+            System.Console.WriteLine($"Scheduler Id: {scheduler.SchedulerInstanceId}");
+
+            var jobKey = JobKey.Create("myJob", "myGroup");
+
+            var hasJob = (await scheduler.GetJobDetail(jobKey)) != null;
+
+            if (!hasJob) await CreateJobsAndTriggers(scheduler);
+
+            await scheduler.Start();
+
+            await Task.Delay(-1);
+        }
+
+        static async Task CreateJobsAndTriggers(IScheduler scheduler)
+        {
+            var job = JobBuilder.Create<ExampleJob>()
+                .WithIdentity("myJob", "myGroup")
+                .Build();
+
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "myGroup")
+                .ForJob(job)
+                .StartNow()
+                .WithCronSchedule("0/5 * * * * ?")
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
